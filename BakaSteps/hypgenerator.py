d161e8a8ef8b148c766e051650b1316e27021805
domenico_c <domenico_c@ff8db5b3-65f4-0310-b8f3-ed11fc615b7d>
2005-07-27 17:48:30 +0000

    Localizzazione dell'update e generatore di ipotesi addizionali.

--- BakaSteps/hypgenerator.py
+++ BakaSteps/hypgenerator.py
@@ -14,35 +14,80 @@ il percorso di inserimento dell'update).
 from atom import *
 
 
-def generate_hypotheses(document, atoms, start_at=0, format='HypVar%d'):
+def core(atom):
+	return atom.document.create_atom(
+			atom.element, {'$id': atom.parameters['$id']})
+
+
+def is_var(x):
+	return type(x) is str and x[0].isupper()
+
+
+def is_ground(x):
+	return type(x) is str and not x[0].isupper()
+
+
+class HypGenerator (object):
 	
-	varcount = start_at
-	rv = []
+	def __init__(self, document, start_at=0, format='HypVar%d'):
+		self.document = document
+		self.varcount = start_at
+		self.format = format
 	
-	for atom in atoms:
-		if isinstance(atom, Atom):
-			atom_id = atom.parameters['$id']
-			for param, value in atom.parameters.iteritems():
-				if param == '$id':
-					continue
-				else:
-					new_var = format % varcount
-					varcount += 1
-					hyp_a = document.create_atom(atom.element,
-						{'$id': atom_id, param: new_var})
-					hyp_b = MathAtom('~=', new_var, value)
-					rv.append([hyp_a, hyp_b])
-		elif isinstance(atom, MathAtom):
-			negate_op = {'<': '>=', '<=': '>', '=': '~=', '~=': '=', '>=': '<',
+	def new_var(self):
+		rv = self.format % self.varcount
+		self.varcount += 1
+		return rv
+
+	def generate_hypotheses(self, atoms):
+		'''
+		Riformula la conoscenza espressa negli atomi passati come 
+		parametro sotto forma di un insieme di denials.
+		'''
+		
+		negate_op = {'<': '>=', '<=': '>', '=': '~=', '~=': '=', '>=': '<',
 				'>': '<='}
-			if atom.op in negate_op:
-				rv.append([MathAtom(negate_op(atom.op), *atom.parameters)])
-	
-	return rv
+		
+		aux_atoms = []
+		vars = {}
+		hypotheses = []
+		
+		for atom in atoms:
+			if isinstance(atom, AuxAtom) and atom.op in negate_op:
+				neg = atom.copy()
+				neg.op = negate_op[atom.op]
+				aux_atoms.append(neg)
+			else:
+				for param, value in atom.parameters.iteritems():
+					if is_ground(value) and param != '$id':
+						premise = core(atom)
+						nv = self.new_var()
+						premise.parameters[param] = nv
+						comparison = AuxAtom('~=', nv, value)
+						hypotheses.append([premise, comparison])
+					else:
+						if value not in vars:
+							vars[value] = (core(atom), param)
+		
+		for atom in aux_atoms:
+			hyp = []
+			for p in atom.parameters:
+				if is_var(p):
+					definition, position = vars[p]
+					premise = definition.copy()
+					premise.parameters[position] = p
+					hyp.append(premise)
+			hyp.append(atom)
+			hypotheses.append(hyp)
+		
+		for denial in hypotheses:
+			print '<-', ', '.join(map(str, denial))
+		
+		return hypotheses
 
 
 if __name__ == '__main__':
-
+	
 	from testhelper import *
 	
 	n = sdd_azienda.create_atom
@@ -53,7 +98,7 @@ if __name__ == '__main__':
 		n('reparto', {'$id': '?D', '$parent': '?S', 'nome': '"R&D"'})
 	]
 	
-	hyp = generate_hypotheses(sdd_azienda, descr)
+	for i in descr: print i
 	
-	for denial in hyp:
-		print '<-', ', '.join(map(str, denial))
+	hyp = HypGenerator(sdd_azienda).generate_hypotheses(descr)
+	
